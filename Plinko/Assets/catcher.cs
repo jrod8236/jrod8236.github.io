947bdcfc1570b150341a6be408f0467c6a60b62a
jrod8236 <56264874+jrod8236@users.noreply.github.com>
2019-10-28 09:49:36 -0500

    Plinko game
    Drop balls to get a high score

---
+++ Plinko/Assets/catcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catcher : MonoBehaviour
+{
+    public int scoreValue;
+    public top dropper;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //give dropperScript the points to add to score
+        dropper.AddScore(scoreValue); 
+
+        Destroy(other.gameObject);
+
+    }
+}
